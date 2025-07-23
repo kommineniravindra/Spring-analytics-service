8baf65d1a75d498f98748756c76f35712a730253
kommineniravindra <kommineniravindra99@gmail.com>
2025-07-23 17:44:39 +0530

    update

--- src/main/java/com/sathya/feign/OrderFeignClient.java
+++ src/main/java/com/sathya/feign/OrderFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.sathya.model.OrderDTO;
 
-@FeignClient(name = "order-service")
+@FeignClient(name = "order-service" , url ="https://spring-order-service.onrender.com")
 public interface OrderFeignClient {
 
     @GetMapping("/api/orders/all")
